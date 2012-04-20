b68cdd662928fc654166103d7b68faded01964c6
Iwan91 <kamil3453@yahoo.com>
2012-04-20 19:28:20 +0200

    fixed some bugs

--- Sources/Client/Unit1.dfm
+++ Sources/Client/Unit1.dfm
@@ -1,6 +1,6 @@
 object Form1: TForm1
-  Left = 404
-  Top = 260
+  Left = 405
+  Top = 261
   Width = 500
   Height = 317
   HorzScrollBar.Visible = False
--- Sources/Server/lib/Classes.py
+++ Sources/Server/lib/Classes.py
@@ -6,16 +6,27 @@ class rekord():
 
 class DB:
   conn = None
-      
+  server = None
+  database = None
+  username = None
+  pswd = None
+  
   def connect(self,server, database, username, pswd):
-    self.conn = MySQLdb.connect(host=server,user=username,passwd=pswd,db=database,use_unicode=True, charset="utf8")
+    self.server=server
+    self.database=database
+    self.username=username
+    self.pswd=pswd
+    self.conn = MySQLdb.connect(host=self.server,user=self.username,passwd=self.pswd,db=self.database,use_unicode=True, charset="utf8")
+    
+  def reconnect(self):
+    self.conn = MySQLdb.connect(host=self.server,user=self.username,passwd=self.pswd,db=self.database,use_unicode=True, charset="utf8")
 
   def query(self, sql,parametry=()):
     try:
       cursor = self.conn.cursor()
       cursor.execute(sql,parametry)
     except (AttributeError, MySQLdb.OperationalError):
-      self.connect()
+      self.reconnect()
       cursor = self.conn.cursor()
       cursor.execute(sql,parametry)
     return cursor
